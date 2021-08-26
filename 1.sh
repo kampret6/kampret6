f2518d4ed2aa8bc5589555087b9acb418bc72e4f
kampret6 <89581392+kampret6@users.noreply.github.com>
2021-08-27 01:50:47 +0700

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,9 @@
+echo -e "\e[34mwelcome back \e[0m";
+echo -e "\e[35mmy termux \e[0m";
+read -p 'jawablah pertanyaan ini' userinput1;
+#
+5  × 2
+if [ "${@:-}" = "20" ]
+then
+	echo -e "\e[33mbenarrr.\e[0m";
+fi
